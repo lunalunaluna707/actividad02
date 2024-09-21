143e32dbb1410cce5a7ed01fcc1d76c5e2467202
lunalunaluna707 <vazquezmariana125670@gmail.com>
2024-09-21 11:25:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,33 @@ def hello_world():
 def formulario():
     return render_template('formulario.html')
 
+@app.route('/editar/<int:id>', methods=['GET','POST'])
+def editar(id, message=None):
+    try:
+        con = mysql.connector.connect(
+        host="185.232.14.52",
+        database="u760464709_tst_sep",
+        user="u760464709_tst_sep_usr",
+        password="dJ0CIAFF="
+        )
+        cursor = con.cursor()
+        sql= "SELECT * FROM tst0_experiencias WHERE Id_Experiencia= %s"
+        cursor.execute(sql,(id,))
+        registros = cursor.fetchall()
+        if len(registros)==0:
+            message="no se encontro registro con este ID"
+            return render_template('editar.html', message=message, registros=[])
+        print("Registros obtenidos:", registros)
+    except mysql.connector.Error as err:
+        print("Error al conectar con MySQL:", err)
+        registros = []  
+    finally:
+        if con.is_connected():
+            cursor.close()
+            con.close()
+    
+    return render_template('editar.html', registros=registros, message=message)
+
 @app.route("/tabla")
 def index():
     con = mysql.connector.connect(
@@ -68,11 +95,11 @@ def evento():
             cursor.execute(sql, (nombre,comentario,calificacion))
             con.commit()
             con.close()
-            message = 'Usuario agregado exitosamente!'
+            message = '¡Encuesta enviada exitosamente!'
             pusher_client.trigger('my-channel', 'my-event', {'message': f'Nuevo evento: {nombre}, {comentario}, {calificacion}'})
         except mysql.connector.Error as err:
             print("error al conectar msql:", err)
-            message="Error al insertar usuario"
+            message="Error al enviar encuesta"
         finally:
              if con.is_connected():
                 cursor.close()
@@ -118,7 +145,8 @@ def eliminar(id):
         sql = "DELETE FROM tst0_experiencias WHERE Id_Experiencia= %s"
         cursor.execute(sql,(id,))
         con.commit()
-        print(cursor.rowcount, "records deleted")
+        print(cursor.rowcount, "Eliminacion exitosa")
+        pusher_client.trigger('my-channel', 'my-event', {'message': f'Nuevo evento: {id}'})
         return '', 200
     except mysql.connector.Error as err:
         print("Error al eliminar con MySQL:", err)
@@ -129,7 +157,36 @@ def eliminar(id):
             con.close()
             
     #return redirect(url_for('vistatabla'))
-
+@app.route('/actualizar/<int:id>', methods=['GET','POST'])
+def actualizar(id):
+    if request.method=='POST':
+        Nuevo_nombre=request.form['txtnombreApellido']
+        Nuevo_comentario=request.form['txtcomentario']
+        Nuevo_calificacion=request.form['txtcalificacion']
+        try:
+            con = mysql.connector.connect(
+            host="185.232.14.52",
+            database="u760464709_tst_sep",
+            user="u760464709_tst_sep_usr",
+            password="dJ0CIAFF="
+            )
+            cursor = con.cursor()
+            sql = "UPDATE tst0_experiencias SET Nombre_Apellido=%s, Comentario=%s, Calificacion=%s WHERE Id_Experiencia= %s"
+            
+            cursor.execute(sql,(Nuevo_nombre, Nuevo_comentario, Nuevo_calificacion, id,))
+            con.commit()
+            con.close()     
+            message = '¡Encuesta modificada exitosamente!'
+            pusher_client.trigger('my-channel', 'my-event', {'message': f'Nuevo evento: {Nuevo_nombre}, {Nuevo_comentario}, {Nuevo_calificacion}'})
+        except mysql.connector.Error as err:
+            print("Error al modificar con MySQL:", err)
+            message="Error al modificar"
+        finally:
+            if con.is_connected():
+                cursor.close()
+                con.close()
+        return editar(id, message=message)
+    return editar(id)    
 
 if __name__ == '__main__':
     app.run(debug=True)
